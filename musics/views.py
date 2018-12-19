ac812c44fe00f34131a145287e792534b78f9ed1
gr11n <gr11n@kimo.com>
2018-12-19 23:20:16 +0800

    Initial commit

---
+++ musics/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+
+
+# Create your views here.
+def hello_view(request):
+    return render(request, 'hello_django.html', {
+        'data': "Hello Django ",
+    })
